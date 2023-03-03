7c83e7e3918463170d5d036f705b96325de63045
Aleksandar <milicalex@gmail.com>
2023-03-03 13:01:31 +0100

    cast latitude and longitude to float through User model

--- api/app/Http/Controllers/Api/UserController.php
+++ api/app/Http/Controllers/Api/UserController.php
@@ -4,7 +4,6 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use App\Http\Resources\UserRecourse;
-use App\Models\User;
 use App\Repositories\UserRepository\IUserRepository;
 use Illuminate\Http\Request;
 
